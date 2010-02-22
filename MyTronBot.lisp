b52cfb988534e86a4734d8b4db8c2d7f61d8e97f
Matei Conovici <cmatei@livigent.ro>
2010-02-22 21:25:01 +0200

    Use conditions to abort

--- MyTronBot.lisp
+++ MyTronBot.lisp
@@ -19,15 +19,18 @@
 (defparameter *fixed-depth* nil)
 (defparameter *time-expired* nil)
 
-(setq *verbose* t)
+(setq *verbose* nil)
+
+(define-condition out-of-time () ())
 
 ;; a queue
 (defstruct q
   (last nil)
   (elements nil))
 
+(declaim (inline enqueue dequeue))
 (defun enqueue (q item)
-  (cond ((or (null (q-last q)) (null (q-elements q)))
+  (cond ((null (q-elements q))
 	 (setf (q-last q) (cons item nil)
 	       (q-elements q) (q-last q)))
 	 (t
@@ -37,6 +40,7 @@
 (defun dequeue (q)
   (pop (q-elements q)))
 
+(declaim (inline neighbors-of))
 (defun neighbors-of (tron pos)
   (let ((x (car pos))
 	(y (cadr pos))
@@ -61,6 +65,7 @@
 
 
 (defparameter +tile-tie+ 100000)
+
 (declaim (inline tile-not-visited))
 (defun tile-not-visited (scoreboard x y who)
   (declare (type fixnum x y who)
@@ -134,11 +139,8 @@
 		    when child do
 		      (let ((cval 0)
 			    (nval (negamax child (1- depth) (- beta) (- alpha) (- color))))
-			(declare (type (or symbol fixnum) nval)
+			(declare (type fixnum nval)
 				 (type fixnum cval))
-
-			(when (eq nval ':aborted)
-			  (return-from nmax nval))
 
 			(setq cval (- nval))
 
@@ -149,29 +151,30 @@
 			  (return-from nmax alpha)))
 		    finally (return-from nmax alpha)))))
 
-    (if *time-expired*
-	':aborted
-	(let ((val (if (= color 1)
-		       (cond				; i am to move and ...
+    (when *time-expired*
+      (signal 'out-of-time))
+    
+    (let ((val (if (= color 1)
+		   (cond				; i am to move and ...
+		     
+		     ((equal (tron-p1 node)		; we're in the same spot
+			     (tron-p2 node))
+		      +draw+)
 
-			 ((equal (tron-p1 node)		; we're in the same spot
-				 (tron-p2 node))
-			  +draw+)
+		     ((player-stuck-p node 1)		; i am stuck
+		      (if (player-stuck-p node -1)
+			  +draw+			; ... but he is too
+			  +defeat+))			; ... or not :-(
 
-			 ((player-stuck-p node 1)		; i am stuck
-			  (if (player-stuck-p node -1)
-			      +draw+			; ... but he is too
-			      +defeat+))			; ... or not :-(
+		     ((player-stuck-p node -1)	; we've got him stuck :-)
+		      +victory+)
 
-			 ((player-stuck-p node -1)	; we've got him stuck :-)
-			  +victory+)
-
-			 (t				; got work to do
-			  (nmax-loop node depth color alpha beta)))
+		     (t				; got work to do
+		      (nmax-loop node depth color alpha beta)))
 
 					; opponent reply move
-		       (nmax-loop node depth color alpha beta))))
-	  val))))
+		   (nmax-loop node depth color alpha beta))))
+	  val)))
 
 (defun negamax-toplevel (node depth)
   (declare (type fixnum depth))
@@ -181,10 +184,8 @@
        as child = (make-child-tron node move 1)
        when child do
 	 (let ((nval (negamax child (the fixnum (1- depth)) +defeat+  +victory+ -1)))
-	   (declare (type (or symbol fixnum) nval))
-	   (if (eq nval ':aborted)
-	       (return-from negamax-toplevel ':aborted)
-	       (setq nm (cons (list move (the fixnum (- nval))) nm)))))
+	   (declare (type fixnum nval))
+	       (setq nm (cons (list move (the fixnum (- nval))) nm))))
     (sort nm #'> :key (lambda (x) (cadr x)))))
 
 
@@ -216,112 +217,74 @@
 		     (ff-area (1+ x) y)
 		     (ff-area x (1+ y)))))))
 
-(defun flood-fill (node depth)
-  (declare (optimize (speed 3) (safety 0) (debug 0))
-	   (type fixnum depth)
-	   (ftype (function (tron fixnum) (or symbol fixnum)) flood-fill))
-  (let ((best 0))
-    (declare (type fixnum best))
-    (cond (*time-expired*
-	   ':aborted)
-	  ((zerop depth)
-	   (count-reachables node 1))
-	  (t
-	   (loop
-	      for move in '(:left :up :right :down)
-	      as child = (make-child-tron node move 1)
-	      when child
-	      do (let ((val (flood-fill child (1- depth))))
-		   (declare (type (or symbol fixnum) val))
-		   (when (eq val ':aborted)
-		     (return-from flood-fill ':aborted))
-
-		   (when (> val best)
-		     (setq best val))
-
-;		   (logmsg move ", val " val ", best " best "~%")
-		   ))
-	   (incf best)
-	   best))))
-
-(defun fill-toplevel (node depth)
-  (declare (type fixnum depth)
-	   (ftype (function (tron fixnum) (or symbol fixnum)) fill-toplevel))
-  (let ((fm nil))
-    (loop
-       for move in '(:left :up :right :down)
-       as child = (make-child-tron node move 1)
-       when child do
-	 (let ((val (flood-fill child (1- depth))))
-	   (declare (type (or symbol fixnum) val))
-	   (logmsg "toplevel fill: move " move ", val " val ", depth " depth "~%")
-	   (if (eq val ':aborted)
-	       (return-from fill-toplevel ':aborted)
-	       (setq fm (cons (list move val) fm)))))
-    (sort fm #'> :key (lambda (x) (cadr x)))))
-
-
 (defun iterative-deepening (node start-depth step proc)
   (declare (type fixnum start-depth step))
-  (let ((moves nil))
-    (loop
-       for depth fixnum = start-depth then (incf depth step)
-       as newmoves = (funcall proc node depth)
-       when (not (eq newmoves ':aborted)) do (setq moves newmoves)
-       until (or *fixed-depth*
-		 *time-expired*
-;		 (end-of-game moves)
-		 (> depth 5000)) ;; this is to catch the above until a fix
-       finally (logmsg "DEPTH: " depth ", moves " (dir-and-score moves) "~%"))
+  (let ((moves nil)
+	(depth-reached 0))
+    (declare (type fixnum depth-reached))
+    
+    (handler-case
+	(loop
+	   for depth fixnum = start-depth then (incf depth step)
+	   do (setq moves (funcall proc node depth)
+		    depth-reached depth)
+	   until (or *fixed-depth*
+		     *time-expired*
+		     ;(end-of-game moves)
+		     (> depth 5000))) ;; this is to catch the above until a fix
+      (out-of-time ()))
+
+    (logmsg "DEPTH: " depth-reached ", moves " (dir-and-score moves) "~%")
     moves))
+      
 
 
-(defun players-separated-p (tron)
-  (declare (optimize (speed 3) (safety 0) (debug 0)))
-  (let ((map (copy-tron-map (tron-map tron)))
-	(x (x-of tron 1))
-	(y (y-of tron 1)))
-    (declare (type (simple-array character (* *)) map)
-	     (type fixnum x y))
-
-    (labels ((ff-area (x y)
-	       (declare (type fixnum x y)
-			(ftype (function (fixnum fixnum) fixnum) ff-area))
-
-	       (cond ((char= (aref map x y) #\2)
-		      (return-from players-separated-p nil))
-		     ((not (empty-square-p map x y))
-		      nil)
-		     (t
-		      (setf (aref map x y) #\#)
-		      (or
-		       (ff-area (1- x) y)
-		       (ff-area x (1- y))
-		       (ff-area (1+ x) y)
-		       (ff-area x (1+ y)))))))
-
-      (not (or (ff-area (1- x) y)
-	       (ff-area x (1- y))
-	       (ff-area (1+ x) y)
-	       (ff-area x (1+ y)))))))
-
-(defun distance-to-opponent (tron)
-  (let ((x1 (x-of tron 1))
-	(y1 (y-of tron 1))
-	(x2 (x-of tron -1))
-	(y2 (y-of tron -1)))
-    (declare (type fixnum x1 x2 y1 y2))
-    (truncate (sqrt (+ (expt (- x1 x2) 2)
-		       (expt (- y1 y2) 2))))))
-
-(defun break-ties (tron moves metric)
-  (let ((m (loop
-	      for move in moves
-	      as child = (make-child-tron tron (car move) 1)
-	      collecting (list (funcall metric child) move))))
-
-    (setq m (sort m #'> :key #'(lambda (x) (car x))))
-    (mapcar #'(lambda (x) (cadr x)) m)))
+;; (defun players-separated-p (tron)
+;;   (declare (optimize (speed 3) (safety 0) (debug 0)))
+;;   (let ((map (copy-tron-map (tron-map tron)))
+;; 	(x (x-of tron 1))
+;; 	(y (y-of tron 1)))
+;;     (declare (type (simple-array character (* *)) map)
+;; 	     (type fixnum x y))
+;; 
+;;     (labels ((ff-area (x y)
+;; 	       (declare (type fixnum x y)
+;; 			(ftype (function (fixnum fixnum) fixnum) ff-area))
+;; 
+;; 	       (cond ((char= (aref map x y) #\2)
+;; 		      (return-from players-separated-p nil))
+;; 		     ((not (empty-square-p map x y))
+;; 		      nil)
+;; 		     (t
+;; 		      (setf (aref map x y) #\#)
+;; 		      (or
+;; 		       (ff-area (1- x) y)
+;; 		       (ff-area x (1- y))
+;; 		       (ff-area (1+ x) y)
+;; 		       (ff-area x (1+ y)))))))
+;; 
+;;       (not (or (ff-area (1- x) y)
+;; 	       (ff-area x (1- y))
+;; 	       (ff-area (1+ x) y)
+;; 	       (ff-area x (1+ y)))))))
+;; 
+;; (defun distance-to-opponent (tron)
+;;   (let ((x1 (x-of tron 1))
+;; 	(y1 (y-of tron 1))
+;; 	(x2 (x-of tron -1))
+;; 	(y2 (y-of tron -1)))
+;;     (declare (type fixnum x1 x2 y1 y2))
+;;     (truncate (sqrt (+ (expt (- x1 x2) 2)
+;; 		       (expt (- y1 y2) 2))))))
+;; 
+;; (defun break-ties (tron moves metric)
+;;   (let ((m (loop
+;; 	      for move in moves
+;; 	      as child = (make-child-tron tron (car move) 1)
+;; 	      collecting (list (funcall metric child) move))))
+;; 
+;;     (setq m (sort m #'> :key #'(lambda (x) (car x))))
+;;     (mapcar #'(lambda (x) (cadr x)) m)))
 
 (defun decide-move (tron)
   (let* ((start-time (get-internal-real-time))
@@ -333,7 +296,7 @@
      +time-available+)
 
     (setq *time-expired* nil)
-    (setq *players-separated* (players-separated-p tron))
+;;    (setq *players-separated* (players-separated-p tron))
 
 ;    (setq moves
 ;	  (if *players-separated*
